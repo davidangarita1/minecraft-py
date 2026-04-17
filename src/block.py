070de5fa9878d85ece26c7925fb6a8c968ccde7e
David Angarita <davidangarita.sistemas@gmail.com>
2026-04-16 21:12:58 -0500

    fix(block): defer texture loading until after Ursina initialization

--- src/block.py
+++ src/block.py
@@ -2,18 +2,25 @@
 
 from ursina import Entity, load_texture
 
-block_textures = {
-    "grass":      load_texture("assets/textures/groundEarth.png"),
-    "dirt":       load_texture("assets/textures/groundMud.png"),
-    "stone":      load_texture("assets/textures/wallStone.png"),
-    "bedrock":    load_texture("assets/textures/stone07.png"),
-    "snow":       load_texture("assets/textures/groundSnow.png"),
-    "ice":        load_texture("assets/textures/ice01.png"),
-    "lava":       load_texture("assets/textures/lava01.png"),
-    "brick":      load_texture("assets/textures/wallBrick01.png"),
-    "gravel":     load_texture("assets/textures/stone04.png"),
-    "cobblestone":load_texture("assets/textures/Stone01.png"),
+TEXTURE_PATHS = {
+    "grass": "assets/textures/groundEarth.png",
+    "dirt": "assets/textures/groundMud.png",
+    "stone": "assets/textures/wallStone.png",
+    "bedrock": "assets/textures/stone07.png",
+    "snow": "assets/textures/groundSnow.png",
+    "ice": "assets/textures/ice01.png",
+    "lava": "assets/textures/lava01.png",
+    "brick": "assets/textures/wallBrick01.png",
+    "gravel": "assets/textures/stone04.png",
+    "cobblestone": "assets/textures/Stone01.png",
 }
+
+block_textures = {}
+
+
+def load_block_textures():
+    for name, path in TEXTURE_PATHS.items():
+        block_textures[name] = load_texture(path)
 
 
 class Block(Entity):
@@ -26,5 +33,4 @@ class Block(Entity):
             origin_y=0.5,
             collider="box",
         )
-        self.block_type = block_type
         self.block_type = block_type
